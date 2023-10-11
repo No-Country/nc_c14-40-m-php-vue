fcfb90d4b6b631a94ff7a5e54f73326dd85dd1bf
stixbunny <milenko.castillo@gmail.com>
2023-10-11 19:00:50 -0300

    Navbar missing search, folder reestructure

--- frontend/vue-front/src/App.vue
+++ frontend/vue-front/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import { RouterView } from "vue-router";
-import TheNavbar from "./components/TheNavbar.vue";
+import TheNavbar from "./components/navbar/TheNavbar.vue";
 </script>
 
 <template>
   <v-app>
     <TheNavbar />
     <h1 class="text-center">Veutify 3</h1>
-
     <v-container>
       <RouterView />
     </v-container>
--- frontend/vue-front/src/stores/user.js
+++ frontend/vue-front/src/stores/user.js
@@ -2,7 +2,8 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
-  const isAuthenticated = ref(false)
+  const isAuthenticated = ref(true)
+  const fullname = "Cosme Fulanito"
 
-  return { isAuthenticated }
+  return { isAuthenticated, fullname }
 })
