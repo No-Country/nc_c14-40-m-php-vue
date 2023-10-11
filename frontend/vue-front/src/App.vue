ac47599dec96a65e7901489e923157f597cc6370
Milenko Castillo <milenko.castillo@gmail.com>
2023-10-11 10:52:06 -0300

    basic navbar

--- frontend/vue-front/src/App.vue
+++ frontend/vue-front/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
+import TheNavbar from "./components/TheNavbar.vue";
 </script>
 
 <template>
   <v-app>
+    <TheNavbar />
     <h1 class="text-center">Veutify 3</h1>
 
     <v-container>
---
+++ frontend/vue-front/src/components/TheNavbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar color="orange darken-1">
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn>
+        Más Populares
+      </v-btn>
+      <v-btn>
+        Cerca de Mí
+      </v-btn>
+      <v-btn>
+        Nosotros
+      </v-btn>
+      <v-btn prepend-icon="mdi-account">
+        <v-icon>mdi-heart</v-icon>
+        Iniciar sesión
+      </v-btn>
+    </v-app-bar>
+    <v-container>
+      <RouterView />
+    </v-container>
+  </v-app>
+</template>
