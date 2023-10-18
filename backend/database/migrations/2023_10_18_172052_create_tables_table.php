17ff54515686ca89442abcf53e7eeab8a088c9e9
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-18 20:31:40 +0200

    Foreign keys can be nullable on create_tables_table

--- backend/database/migrations/2023_10_18_172052_create_tables_table.php
+++ backend/database/migrations/2023_10_18_172052_create_tables_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tables', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('restaurant_id');
+            $table->unsignedBigInteger('restaurant_id')->nullable();
             $table->integer('capacity');
             $table->boolean('isTaken?');
             
