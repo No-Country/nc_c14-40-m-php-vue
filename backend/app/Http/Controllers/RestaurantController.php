583c077957a7124e86a7a77e73d5cc4c2b12e42c
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-18 19:40:29 +0200

    Login & Register implemented w/ Laravel Passport

--- backend/app/Http/Controllers/RestaurantController.php
+++ backend/app/Http/Controllers/RestaurantController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
+use Illuminate\Support\Facades\Auth;
 
 class RestaurantController extends Controller
 {
     public function index(){
         return Restaurant::all();
     }
+
+    public function showUserRestaurants($id){
+        if(Auth::user()->id === intval($id)){
+            $restaurants = Restaurant::where('user_id', intval($id))->get();
+            $count_restaurants = $restaurants->count();
+            return response(['number_of_restaurants' => $count_restaurants, 'restaurants' => $restaurants], 200);
+        }else{
+            return response(['error' => "Access denied!"], 403);
+        }
+    }
+
 }
