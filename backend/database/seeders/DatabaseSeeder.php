58c6d913a1e603967d7d4402c0d8c56d286c168b
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-21 23:12:03 +0200

    UserSeeder -> UserFactory

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,23 +12,18 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        
+    {        
+        User::factory(10)->create(); // <-- 10 users ≈ $this->call(UserSeeder::class) x 10;
+       
         $this->call([
-            UserSeeder::class, // <-- 2 USERS
-            RestaurantSeeder::class, // <-- 2 RESTAURANTS
-            // RestaurantJSONSeeder::class, // <-- +3.000 RESTAURANTS
-            TablesSeeder::class, // <-- nº de Restaurants * nº de la columna 'tables_number' de cada Restaurant
-            AvailableDateSeeder::class,
-            ReservationTableSeeder::class, // registro de reservaciones <-- PUEDE FALLAR LA MIGRACIÓN, SI FALLA VOLVER A INTENTAR!!!
+            // UserSeeder::class, // <-- 2 USERS
+            // RestaurantSeeder::class, // <-- 2 RESTAURANTS
+            // // RestaurantJSONSeeder::class, // <-- +3.000 RESTAURANTS
+            // TablesSeeder::class, // <-- nº de Restaurants * nº de la columna 'tables_number' de cada Restaurant
+            // AvailableDateSeeder::class,
+            // ReservationTableSeeder::class, // registro de reservaciones <-- PUEDE FALLAR LA MIGRACIÓN, SI FALLA VOLVER A INTENTAR!!!
 
-            ReviewsSeeder::class
+            // ReviewsSeeder::class
         ]);
 
     }
