1fdaaa79e2bb152484381fe4622a76e46895c052
Richard Ailton Alfaro Manzano <richardalfaromanznao@gmail.com>
2023-10-23 13:06:01 -0500

    deleting some references app config

--- backend/config/app.php
+++ backend/config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Stevebauman\Location\LocationServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -184,7 +183,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Location' => Stevebauman\Location\Facades\Location::class,
 
     ])->toArray(),
 
